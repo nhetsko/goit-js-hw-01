243183771bfcf4fd9953b6f5364053e03521e62e
nhetsko <nadiahecko@gmail.com>
2023-10-12 18:42:07 +0300

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,13 @@
+// Task 3
 function getElementWidth(content, padding, border) {
-return Number.parseFloat(content + 2 * padding + 2 * border);   
+const contentWidth = Number.parseFloat(content);
+const paddingWidth = Number.parseFloat(padding);
+const borderWidth = Number.parseFloat(border);
+
+  const elementWidth = contentWidth + 2*paddingWidth + 2*borderWidth;
+  return elementWidth;
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
