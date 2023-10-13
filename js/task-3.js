59ee8f560eecd045ec51a8ad4caad621672be38a
nhetsko <nadiahecko@gmail.com>
2023-10-13 17:28:29 +0300

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
 "use strict"
 // Task 3
 function getElementWidth(content, padding, border) {
-const contentWidth = Number.parseFloat(content);
-const paddingWidth = Number.parseFloat(padding);
-const borderWidth = Number.parseFloat(border);
+const contentWidth = parseFloat(content);
+const paddingWidth = parseFloat(padding);
+const borderWidth = parseFloat(border);
 
   const elementWidth = contentWidth + 2*paddingWidth + 2*borderWidth;
   return elementWidth;
